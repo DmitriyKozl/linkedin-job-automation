1204372a119b98dc09f6b5e6c44f5e6c1364bd3f
DmitriyKozl <dmitriy.kozlov@student.hogent.be>
2023-10-11 12:38:51 +0200

    searchin for job name skills company name location job type

--- main.py
+++ main.py
@@ -51,6 +51,8 @@ time.sleep(2)
 job_search.send_keys("software developer")
 time.sleep(2)
 job_search.send_keys(Keys.ENTER)
+page = 1
+job_dictionary = {}
 for i in range(6):
     time.sleep(2)
     jobs_list = driver.find_elements(By.CSS_SELECTOR, ".job-card-container--clickable")
@@ -59,13 +61,39 @@ for i in range(6):
     time.sleep(5)
     for card in jobs_list:
         try:
+            time.sleep(8)
             card.click()
             print(f"Job clicked")
             time.sleep(2)
+            job_name = driver.find_element(By.XPATH,
+                                           "/html/body/div[6]/div[3]/div[4]/div/div/main/div/div[2]/div/div[2]/div[1]/div/div[1]/div/div[1]/div[1]/div[1]/a/h2")
+            job_skills = driver.find_element(By.XPATH,
+                                             "/html/body/div[6]/div[3]/div[4]/div/div/main/div/div[2]/div/div[2]/div[1]/div/div[6]/section[2]/div/div/div/div/a")
+            company_name = driver.find_element(By.XPATH,
+                                               "/html/body/div[6]/div[3]/div[4]/div/div/main/div/div[2]/div/div[2]/div[1]/div/section/section/div[1]/div[1]/div/div[2]/div[1]")
+            easy_applied = driver.find_element(By.XPATH,
+                                               "/html/body/div[6]/div[3]/div[4]/div/div/main/div/div[2]/div/div[2]/div[1]/div/div[1]/div/div[1]/div[1]/div[4]/div/div/div/button")
+            link_to_application_form = driver.find_element(By.XPATH,
+                                                           " /html/body/div[6]/div[3]/div[4]/div/div/main/div/div[2]/div/div[2]/div[1]/div/div[1]/div/div[1]/div[1]/div[4]/div/div/div/button")
+            location = driver.find_element(By.XPATH,
+                                           " /html/body/div[6]/div[3]/div[4]/div/div/main/div/div[2]/div/div[2]/div[1]/div/div[1]/div/div[1]/div[1]/div[2]/div/text()")
+            job_type = driver.find_element(By.XPATH,
+                                           "/html/body/div[6]/div[3]/div[4]/div/div/main/div/div[2]/div/div[2]/div[1]/div/div[1]/div/div[1]/div[1]/div[3]/ul/li[1]/span")
+            # job_dictionary[job_name] = {
+            #     "skills": job_skills.text,
+            #     "company": company_name.text,
+            #     "location": location.text,
+            #     "type": job_type.text
+            # }
+            print(f"skills: {job_skills.text},company: {company_name.text}, type: {job_type.text}")
+
 
         except StaleElementReferenceException:
             print("Stale element reference. Skipping.")
-
+    # driver.find_elements("xpath", f"//button[@aria-label='Page {page}']")[0].click()
+    # page += 1
+print(job_dictionary)
+time.sleep(5)
 driver.quit()
 
 # skills:/html/body/div[6]/div[3]/div[4]/div/div/main/div/div[2]/div/div[2]/div[1]/div/div[6]/section[2]/div/div/div/div/a
